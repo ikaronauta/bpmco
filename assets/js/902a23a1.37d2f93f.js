30b7deb1e07dae88ff5d7f4ea0f45e23601b0b22
ikaronauta <jaortiz0496@misena.edu.co>
2022-08-04 09:11:19 -0500

    Deploy website - based on 35666b1688f7be2b1b7f6f641a3f8cc30f14c25b

---
+++ assets/js/902a23a1.37d2f93f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoc_bpmco=self.webpackChunkdoc_bpmco||[]).push([[31],{5745:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-pages","id":"default"}}')}}]);
