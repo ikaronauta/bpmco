35666b1688f7be2b1b7f6f641a3f8cc30f14c25b
ikaronauta <jaortiz0496@misena.edu.co>
2022-08-04 09:10:44 -0500

    update projectName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "ikaronauta", // Usually your GitHub org/user name.
-  projectName: "doc-bpmco", // Usually your repo name.
+  projectName: "bpmco", // Usually your repo name.
 
   presets: [
     [
